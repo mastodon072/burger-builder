a6058671e8cc60fda817e59b81c23c9df0469959
Bikash Waiba <bikash_065@yahoo.com>
2021-02-26 15:06:12 +0545

    Calculation Ingredients count dynamically to show message

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,20 @@ import classes from './Burger.module.css'
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
       .map((igkey) => {
         return [...Array(props.ingredients[igkey])].map((_, i) => {
           return <BurgerIngredient key={igkey + i} type={igkey}/>;
         });
       })
+      .reduce((arr, el) => {
+          return arr.concat(el);
+      }, [])
 
+  if( transformedIngredients.length < 1 ) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+  console.log(transformedIngredients);
   return (
       <div className={classes.Burger}>
         <BurgerIngredient type='bread-top'/>
