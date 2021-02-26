9e31cca6798afd38aedb87b224a2dd4c8ac1dfeb
Bikash Waiba <bikash_065@yahoo.com>
2021-02-26 14:11:27 +0545

    Add dynamic ingredient component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import styles from './App.module.css';
+import Layout from './components/Layout/Layout'
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 function App() {
   return (
     <div>
+      <Layout>
+        <BurgerBuilder/>
+        <p>Test Layout</p>
+      </Layout>
     </div>
   );
 }
