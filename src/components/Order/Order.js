1a789902050c463c92140abed16bb317826bd2da
Bikash Waiba <bikash_065@yahoo.com>
2021-03-22 17:03:01 +0545

    Show orders

---
+++ src/components/Order/Order.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import classes from './Order.module.css'
+
+const order = (props) => {
+    const ingredients = [];
+    
+    for( let ingredientName in props.ingredients ) {
+        ingredients.push(
+            {
+                name: ingredientName, 
+                amount: props.ingredients[ingredientName]
+            }
+        );
+    }
+    console.log(ingredients);
+
+    const ingredientOutput = ingredients.map(ig => {
+        return (
+            <span 
+                style={{
+                    textTransform: 'capitalize',
+                    display: 'inline-block',
+                    margin: '0 8px',
+                    padding: '5px 10px',
+                    border: '1px solid #ccc'
+                }}
+                key={ig.name}
+                >
+                    {ig.name} ({ig.amount})
+            </span>);
+    });
+
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>Price: <strong>${props.price}</strong></p>
+        </div>
+    );
+}
+
+export default order;
