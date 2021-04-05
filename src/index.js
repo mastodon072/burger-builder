05864f689f978eae6a9e2d5cbec302868066bb6d
Bikash Waiba <bikash_065@yahoo.com>
2021-04-05 15:07:37 +0545

    create action creators

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import {Provider} from 'react-redux';
 import './index.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import reducer from './store/reducer';
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(burgerBuilderReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <React.StrictMode>
