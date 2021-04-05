e24a1f22b72157aefa531b411f79f5008c0205ec
Bikash Waiba <bikash_065@yahoo.com>
2021-04-05 18:06:33 +0545

    Complete reducer

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux'
 
 import CheckoutSummmary from '../../components/Order/CheckoutSummary/CheckoutSummary'
 import ContactData from './ContactData/ContactData'
+import * as actions from '../../store/actions/index'
 
 class Checkout extends Component {
 
@@ -17,21 +18,30 @@ class Checkout extends Component {
     }
 
     render() {
-        return (
+        let summary = <Redirect to="/"/>;
+
+        if(this.props.ings) {
+            const purhcasedRedirect = this.props.purchased ? <Redirect to="/"/> : null;
+            summary = (
             <div>
+                {purhcasedRedirect}
                 <CheckoutSummmary 
                     ingredients={this.props.ings}
                     checkoutCancelled={this.checkoutCancelled}
                     checkoutContinued={this.checkoutContinued}/>
-                    <Route path={this.props.match.url + "/contact-data"} component={ContactData}/>
+                <Route path={this.props.match.url + "/contact-data"} component={ContactData}/>
             </div>
-        )
+            );
+        }
+
+        return summary;
     }
 }
 
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients
+        ings: state.burgerBuilder.ingredients,
+        purchased: state.order.purchased
     }
 }
 
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -6,6 +6,9 @@ import Spinner from '../../../components/UI/Spinner/Spinner'
 import classes from './ContactData.module.css'
 import axios from "../../../axios-orders";
 import Input from '../../../components/UI/Input/Input'
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
+import * as actions from '../../../store/actions/index'
+import { act } from 'react-dom/test-utils';
 
 class ContactData extends Component {
     state= {
@@ -98,8 +101,6 @@ class ContactData extends Component {
           },
         },
         formIsValid: false,
-        loading: false,
-
     }
     orderHandler = (event) => {
         event.preventDefault();
@@ -114,13 +115,9 @@ class ContactData extends Component {
           price: (+this.props.price).toFixed(2), //You would want to calculate price in server in real app
           orderData: formData
         }
-        axios.post('/orders.json', order).then(response => {
-          this.setState({loading: false});
-          this.props.history.push('/');
-        }).catch(error => {
-          console.log(error);
-          this.setState({loading: false, purchasing: false});
-        });
+
+        this.props.onOrderBurger(order);
+
     }
 
     checkValidity(value, rules) {
@@ -194,7 +191,7 @@ class ContactData extends Component {
                   <Button btnType="Success" disabled={!this.state.formIsValid}>Order</Button>
               </form>
             );
-        if(this.state.loading) {
+        if(this.props.loading) {
             form = <Spinner/>;
         }
         return (
@@ -208,9 +205,16 @@ class ContactData extends Component {
 
 const mapStateToProps = state => {
   return {
-    ings: state.ingredients,
-    price: state.totalPrice
+    ings: state.burgerBuilder.ingredients,
+    price: state.burgerBuilder.totalPrice,
+    loading: state.order.loading,
   }
 }
 
-export default connect(mapStateToProps)(ContactData);
+const mapDispatchToProps  = dispatch => {
+  return {
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
