bc7089c7040e37f243d750b3228c2a09db130474
Bikash Waiba <bikash_065@yahoo.com>
2021-04-06 12:44:59 +0545

    Fetch orders(via redux)

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -34,8 +34,25 @@ const reducer = (state = initialState, action) => {
                 loading: false,
                 purchased: true
             };
+        case actionTypes.FETCH_ORDERS_START:
+            return {
+                ...state,
+                loading: true,
+            }    
+        case actionTypes.FETCH_ORDERS_SUCCESS: 
+            return {
+                ...state,
+                loading: false,
+                orders: action.orders
+            }    
+        case actionTypes.FETCH_ORDERS_FAIL:
+            return {
+                ...state,
+                loading: false
+            }
+        default:    
+            return state;
     }
-    return state;
 
 }
 
