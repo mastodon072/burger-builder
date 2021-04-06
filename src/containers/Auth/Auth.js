f426e25d15dc1e5f294a4a9fa3e094bac91fb7b5
Bikash Waiba <bikash_065@yahoo.com>
2021-04-06 15:37:54 +0545

    Signup and get token from backend

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,8 +1,10 @@
 import react, {Component} from 'react';
+import {connect} from 'react-redux';
 
 import Input from '../../components/UI/Input/Input'
 import Button from '../../components/UI/Button/Button'
 import classes from './Auth.module.css'
+import * as actions from '../../store/actions/index'
 
 class Auth extends Component {
     state = {
@@ -83,6 +85,11 @@ class Auth extends Component {
         this.setState({controls: updatedControls});
     }
 
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+    }
+
     render() {
         const formElementsArray = [];
         for( let key in this.state.controls) {
@@ -93,7 +100,7 @@ class Auth extends Component {
         }
 
         let form = (
-            <form onSubmit={this.orderHandler}>
+            <form onSubmit={this.submitHandler}>
                 {formElementsArray.map(formElement => (
                   <Input 
                     key={formElement.id}
@@ -117,4 +124,10 @@ class Auth extends Component {
     }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password)) 
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Auth);
