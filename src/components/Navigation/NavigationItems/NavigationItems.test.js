4f2fc718e30f0c3022013849ae9d39a6cc5dfbb2
Bikash Waiba <bikash_065@yahoo.com>
2021-04-08 10:49:42 +0545

    Testing continue

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -8,8 +8,18 @@ configure({adapter: new Adaptor()});
 
 
 describe('<NavigationItmes />', () => {
+    let wrapper;
+    beforeEach(() => wrapper = shallow(<NavigationItems/>));
+
     it('should render two <NavigationItem /> elements if not authenticated.', () => {
-        const wrapper = shallow(<NavigationItems/>);
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+    it('should render three <NavigationItem /> elements if authenticated.', () => {
+        wrapper.setProps({isAuthenticated: true})
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+    it('should show logout link <NavigationItem /> elements if authenticated.', () => {
+        wrapper.setProps({isAuthenticated: true})
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
     });
 });
